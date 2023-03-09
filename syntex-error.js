b98ddc139a58ab6421da3a36bd873aa6d0989518
md-abir-alahe <mdabiralahe@gmail.com>
2023-03-09 20:37:17 +0600

    first commit

---
+++ syntex-error.js
@@ -0,0 +1,27 @@
+function isBigger(num){
+    if(num>5){
+        return true;
+    }
+    else {
+        return false;
+    };
+}
+
+const result = isBigger(11);
+// console.log(result);
+
+const sum = (a,b) => a+b;
+const res = sum(3,5);
+
+
+const musk = {
+    name: 'elon',
+    age: 52,
+    job: 'tesla'
+
+
+}
+
+console.log(musk.age);
+
+const numbers = [12, 13, 21, 3]
---
+++ type-error.js
@@ -0,0 +1,16 @@
+const numbs = 5;
+// numbs.push(5);
+
+// let address;
+// let address = {};
+let address = {
+    city: 'bbb',
+    postcode: 12
+};
+
+console.log(address?.home?.buidling);
+
+const a = 'abul';
+a = 'babul';
+
+
